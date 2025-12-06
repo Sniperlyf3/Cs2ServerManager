0832f2203685997d040ac0ccb07a2a871731217b
John Doe <mail@example.com>
2025-12-06 16:35:20 +0200

    Fix permissions before trying to start service

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿Setup.Setup setup = new();
+﻿using System.Diagnostics;
+
+Setup.Setup setup = new();
 await setup.PerformBaseSetupAsync();
 
 int portStart = 27015;
@@ -13,8 +15,32 @@ CreateServer("retakes-dust2", "Tokolossie Retakes - Dust 2", "de_dust2");
 
 void CreateServer(string serverName, string description, string map, string? gslt = null)
 {
+    RunCommand("sudo", "chown -R cs2user:cs2user /opt/cs2");
     var path = fsMmanager.CreateOverlay(serverName);
     var service = serviceManager.CreateServiceFile(serverName, path, gslt, portStart++, description, map);
     serviceManager.EnableService(serverName);
     serviceManager.StartService(service);
+}
+
+void RunCommand(string fileName, string arguments)
+{
+    var psi = new ProcessStartInfo
+    {
+        FileName = fileName,
+        Arguments = arguments,
+        RedirectStandardOutput = true,
+        RedirectStandardError = true,
+        UseShellExecute = false
+    };
+
+    using var proc = Process.Start(psi);
+    if (proc != null)
+    {
+        string stdout = proc.StandardOutput.ReadToEnd();
+        string stderr = proc.StandardError.ReadToEnd();
+        proc.WaitForExit();
+
+        if (!string.IsNullOrWhiteSpace(stdout)) Console.WriteLine(stdout);
+        if (!string.IsNullOrWhiteSpace(stderr)) Console.WriteLine(stderr);
+    }
 }
